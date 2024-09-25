d6066c428aeb76b1b6e876f4055e2d743e2b871b
Ijaz Ul Haq <ejazulhaq8281@gmail.com>
2024-09-25 18:00:52 +0500

    Updates

--- assets/404Page-K7gTAOHr.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-BKCSi_yL.js";const r={};function t(_,c){return"404 Not Found"}const o=e(r,[["render",t]]);export{o as default};
---
+++ assets/404Page-moOMGgyc.js
@@ -0,0 +1,1 @@
+import{a4 as e}from"./index-CKozka81.js";const r={};function t(c,n){return"404 Not Found"}const _=e(r,[["render",t]]);export{_ as default};
